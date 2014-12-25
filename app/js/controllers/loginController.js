06c311f3ed6ff5887364d2bc1cc2f505207524f4
kotow <kotow@abv.bg>
2014-12-25 22:44:28 +0200

    Login/Register Controllers

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,3 +1,19 @@
-app.controller('loginController', function($scope, adsData, $log) {
+app.controller('loginController', function($scope, $http) {
+	$scope.myForm = {};
+    $scope.myForm.submitTheForm = function(item, event) {
+       console.log($scope.myForm);
+       var dataObject = {
+	   username: $scope.myForm.username,
+	   password:$scope.myForm.password,
+	   };
+
+       var responsePromise = $http.post("http://softuni-ads.azurewebsites.net/api/user/login", dataObject, {});
+       responsePromise.success(function(dataFromServer, status, headers, config) {
+          console.log(dataFromServer);
+       });
+        responsePromise.error(function(data, status, headers, config) {
+          alert("Submitting form failed!");
+       });
+	}
 
 });
--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -1,3 +1,23 @@
-app.controller('registerController', function($scope, adsData, $log) {
+app.controller('registerController', function($scope, $http) {
+	$scope.myForm = {};
+    $scope.myForm.submitTheForm = function(item, event) {
+       console.log($scope.myForm);
+       var dataObject = {
+	   username: $scope.myForm.username,
+	   password:$scope.myForm.password,
+	   confirmPassword:$scope.myForm.password,
+	   name:$scope.myForm.name,
+	   email:$scope.myForm.email,
+	   phone:$scope.myForm.phone,
+	   townId:1
+	   };
 
+       var responsePromise = $http.post("http://softuni-ads.azurewebsites.net/api/user/register", dataObject, {});
+       responsePromise.success(function(dataFromServer, status, headers, config) {
+          console.log(dataFromServer);
+       });
+        responsePromise.error(function(data, status, headers, config) {
+          alert("Submitting form failed!");
+       });
+	}
 });
