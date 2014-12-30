a9448c31690a5170227847d7c20eba96f2031a32
kotow <kotow@abv.bg>
2014-12-30 20:50:32 +0200

    delteAd

--- app/js/controllers/DeleteAdController.js
+++ app/js/controllers/DeleteAdController.js
@@ -1,4 +1,4 @@
-app.controller('DeleteAdController', function($scope, adsData, $log, $http, $routeParams) {
+app.controller('DeleteAdController', function($scope, adsData, $log, $http, $routeParams, $location) {
 	   $http.defaults.headers.common['Authorization'] = "Bearer " + userSession.getCurrentUser().access_token;
 	var responsePromise = $http.get("http://softuni-ads.azurewebsites.net/api/user/ads/"+$routeParams.adId, {});
        responsePromise.success(function(dataFromServer) {
@@ -8,4 +8,13 @@ app.controller('DeleteAdController', function($scope, adsData, $log, $http, $rou
         responsePromise.error(function(data, status, headers, config) {
           alert("Submitting form failed!");
        });
+	   $scope.deleteAd =  function(){
+	var responsePromise = $http.delete("http://softuni-ads.azurewebsites.net/api/user/ads/"+$routeParams.adId, {});
+       responsePromise.success(function(dataFromServer) {
+         		  $location.path( '/user' );
+   });
+        responsePromise.error(function(data, status, headers, config) {
+          alert("Submitting form failed!");
+       });
+	   }
 });
